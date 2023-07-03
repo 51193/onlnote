9cb1acd1fa50b9471cd4d8c4a0adf8f634b2e51b
crepusculumx <crepusculumx@qq.com>
2023-07-03 14:04:11 +0800

    feat: spring init

---
+++ src/main/java/icu/crepus/crepusserver/CrepusServerApplication.java
@@ -0,0 +1,13 @@
+package icu.crepus.crepusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrepusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrepusServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icu/crepus/crepusserver/CrepusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.crepus.crepusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrepusServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
